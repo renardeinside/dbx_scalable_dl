bfc1965ed05a46fdf715b3edb2941025894e9242
renardeinside <polarpersonal@gmail.com>
2022-03-22 11:28:51 +0100

    fix mlflow logging

--- dbx_scalable_dl/callbacks.py
+++ dbx_scalable_dl/callbacks.py
@@ -7,10 +7,11 @@ from dbx_scalable_dl.controller import ModelController
 class MLflowLoggingCallback(tf.keras.callbacks.Callback):
     def __init__(self, model_name: str):
         self._model_name = model_name
-        self._active_run = mlflow.start_run()
+        with mlflow.start_run() as run:
+            self.parent_run_id = run.info.run_id
 
     def on_epoch_end(self, epoch, logs=None):
-        with self._active_run:
+        with mlflow.start_run(run_id=self.parent_run_id):
             with mlflow.start_run(nested=True):
                 mlflow.set_tag("epoch", epoch)
                 if logs:
@@ -18,6 +19,5 @@ class MLflowLoggingCallback(tf.keras.callbacks.Callback):
                     mlflow.log_metrics(logs)
 
     def on_train_end(self, *_):
-        with self._active_run:
+        with mlflow.start_run(run_id=self.parent_run_id):
             ModelController(self._model_name).register_model(self.model)
-            mlflow.end_run()
