1dcd724204969b44af02eecc7c0faaf0db78e014
renardeinside <polarpersonal@gmail.com>
2022-03-30 16:53:44 +0200

    add high-level imports

--- nocturne/__init__.py
+++ nocturne/__init__.py
@@ -1,1 +1,7 @@
+from nocturne.api.model_builder.tensorflow import (
+    TensorflowModelBuilder,
+    TensorflowModelBuilderInfo,
+)
+from nocturne.api.providers import ConverterProvider, TrainValidationProvider
+
 __version__ = "0.0.1"
